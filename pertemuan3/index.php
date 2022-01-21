337948fdac5acd0f396a7e9d0471e01b0be14ecb
shfxsbl <shifaas1234@gmail.com>
2022-01-21 15:38:36 +0700

    menambahkan file pertemuan3

---
+++ pertemuan3/index.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>Pertemuan 3</head>
+
+<body>
+    <?php
+    $nama = "Shifa Aulia";
+    $NIM = "1904030014";
+    $kelas = "5b";
+    $umur = (0x14);
+    $Alamat = "Kp. Cikoneng Baru RT.04/02 no.173";
+    $Hobi = "Membaca novel";
+    $No_Telp = "081318390752";
+    $Agama = "Islam";
+    ?>
+
+    <h2>Identitas diri</h2>
+    <p>Nama : <?php echo $nama; ?></p>
+    <p>NIM : <?php echo $NIM; ?></p>
+    <p>Kelas : <?php echo $kelas; ?></p>
+    <p>Umur : <?php echo $umur; ?></p>
+    <p>Alamat : <?php echo $Alamat; ?></p>
+    <p>Hobi : <?php echo $Hobi; ?></p>
+    <p>Nomer Telepon : <?php echo $No_Telp; ?></p>
+    <p>Agama : <?php echo $Agama; ?></p>
+</body>
+
+</html>
